d8c7a517c4ad3f8a32c9dbff1e0324c5aec4eed7
jk <ulukaiz@hotmail.com>
2014-04-12 01:39:25 +0200

    init travis

---
+++ build_travis.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+files=( "title" "david" "samuel" "jakob" "andre" )
+echo ">> Starts concating files"
+for file in "${files[@]}"
+do
+  cat "src/${file}.md"
+  echo
+done > union_docu.md
+
+echo ">> Finished concating"
+echo ">> Runs Pandoc"
+
+pandoc --filter pandoc-citeproc --toc --include-in-header src/titlesec.tex union_docu.md -o union_docu.pdf
+rm union_docu.md
+
